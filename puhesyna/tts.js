3fa38b86ddafede7958735f653f20ad184627264
misterbinu <panu.laasonen@aalto.fi>
2016-03-09 13:10:27 +0200

    Create tts.js

---
+++ puhesyna/tts.js
@@ -0,0 +1,4 @@
+var lang = document.getElementById("language").elements;
+var gen = document.getElementById("gender").elements;
+var ptc = document.getElementById("pitch").elements;
+var text = document.getElementById("tts_text").elements;
