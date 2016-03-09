97b56d6b279b9af245ac90808e1c5e1da07c1f15
misterbinu <panu.laasonen@aalto.fi>
2016-03-09 13:15:55 +0200

    Update tts.js

--- puhesyna/tts.js
+++ puhesyna/tts.js
@@ -1,4 +1,22 @@
-var lang = document.getElementById("language").elements;
-var gen = document.getElementById("gender").elements;
-var ptc = document.getElementById("pitch").elements;
-var text = document.getElementById("tts_text").elements;
+function speak {
+    var lang = document.getElementById("language").elements;
+    var gen = document.getElementById("gender").elements;
+    var ptc = document.getElementById("pitch").elements;
+    var text = document.getElementById("tts_text").elements;
+    
+    var langInput, genInput, ptcInput, textInput;
+    
+    for(i = 0; i < lang.length; i++) {
+        if(lang[i].checked == true)
+            langInput = String(lang[i].value);
+    }
+    
+    for(i = 0; i < gen.length; i++) {
+        if(gen[i].checked == true)
+            genInput = String(gen[i].value);
+    }
+    
+    ptcInput = Number(0.017 * ptc[0].value + 0.1);
+    
+    var options = { "lang" : langInput, "gender" : genInput, "pitch" : ptcInput };
+}
